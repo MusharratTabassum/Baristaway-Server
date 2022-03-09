d968742ab176b4e2696e6f26a46b98cc2e4279c1
MusharratTabassum <musharrattabassum710@gmail.com>
2022-03-10 05:26:09 +0600

    database connection added

--- index.js
+++ index.js
@@ -23,3 +23,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("Running port on ", port);
 })
+
+//Database Authentication and Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.agixt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const collection = client.db("BaristaWay").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});

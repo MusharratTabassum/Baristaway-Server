a9ef884949b6f66bf1b5b2b1e7ba1c6a7695d6b8
MusharratTabassum <musharrattabassum710@gmail.com>
2022-03-10 05:18:44 +0600

    server is working

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { MongoClient, Collection } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send("BaristaOnWay server is running")
+})
+
+app.listen(port, () => {
+    console.log("Running port on ", port);
+})

9eb635cbe09eb6fce6873f4fe396c2b21be1c6c9
MusharratTabassum <musharrattabassum710@gmail.com>
2022-03-10 05:54:17 +0600

    GET API is working

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -28,8 +26,36 @@ app.listen(port, () => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.agixt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-    const collection = client.db("BaristaWay").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('successfully connected with server!!');
+        const database = client.db("BaristawayDB");
+        const productsCollection = database.collection("products");
+
+
+        //-------------------------- Product API ------------------------------//
+
+        // GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        //POST API
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            console.log(product);
+            const result = await productsCollection.insertOne(product);
+            console.log(result);
+            res.json(result)
+        });
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
